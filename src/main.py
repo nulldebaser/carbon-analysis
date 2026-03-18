1d3b7cbbb2a19b8be1b0db193d851f337101d117
nulldebaser <alejandropattt@gmail.com>
2026-03-18 18:52:51 -0500

    starting phase or set up of code

---
+++ src/main.py
@@ -0,0 +1,51 @@
+# imports
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# config
+DATA_PATH = "../data/carbonmonitor-global_datas_2025-04-21.csv"
+
+# seed and data assignment
+seed = 811
+
+countries = [
+    "Brazil",
+    "China",
+    "EU27 & UK",
+    "France",
+    "Germany",
+    "India",
+    "Italy",
+    "Japan",
+    "ROW",
+    "Russia",
+    "Spain",
+    "United Kingdom",
+    "United States",
+    "WORLD",
+]
+
+sectors = [
+    "Domestic Aviation",
+    "Ground Transport",
+    "Industry",
+    "International Aviation",
+    "Power",
+    "Residential",
+]
+
+years = [2019, 2020, 2021, 2022, 2023, 2024]
+
+idx_country = seed % len(countries)
+idx_sector = (seed // len(countries)) % len(sectors)
+idx_year = (seed // (len(countries) * len(sectors))) % len(years)
+
+country_asig = countries[idx_country]
+sector_asig = sectors[idx_sector]
+year_asig = years[idx_year]
+
+print("Seed:", seed)
+print("País asignado:", country_asig)
+print("Sector asignado:", sector_asig)
+print("Año asignado:", year_asig)
